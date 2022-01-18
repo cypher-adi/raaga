d0e2310ddcc212d71cac3370772585701a29c4f7
cypher-adi <adityasaha444@gmail.com>
2022-01-18 11:29:26 +0530

    gh-pages changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeNavbar from "./components/HomeNavbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Learn from "./components/Learn";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeNavbar from './components/HomeNavbar';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Learn from './components/Learn';
 
 function App() {
   return (
@@ -15,10 +15,10 @@ function App() {
         <div className="ml-4 mt-3">
           <Router>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/learn" element={<Learn />} />
+              <Route path="/raaga" element={<Home />} />
+              <Route path="/raaga/about" element={<About />} />
+              <Route path="/raaga/contact" element={<Contact />} />
+              <Route path="/raaga/learn" element={<Learn />} />
             </Routes>
           </Router>
         </div>
